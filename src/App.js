c999c3c5abc5fd31f0d885a44f9906efe2b121f1
Rachel Manning <r@rachelmanning.com>
2022-02-12 21:22:29 -0700

    completed router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import { useEffect, useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
-import { DataContext } from './context/DataContext'
+import AlbumView from './components/AlbumView'
+import ArtistView from './components/ArtistView'
+import { Fragment } from 'react/cjs/react.production.min'
 
 function App() {
 	let [search, setSearch] = useState('')
@@ -33,11 +36,19 @@ function App() {
 
 	return (
 		<div>
-			<SearchBar handleSearch={handleSearch} />
 			{message}
-			<DataContext.Provider value={data}>
-				<Gallery />
-			</DataContext.Provider>
+			<Router>
+				<Routes>
+					<Route path="/" element={
+						<Fragment>
+							<SearchBar handleSearch={handleSearch} />
+							<Gallery data={data} />
+						</Fragment>
+					} />
+					<Route path="/album/:id" element={<AlbumView />} />
+					<Route path="/artist/:id" element={<ArtistView />} />
+				</Routes>
+			</Router>
 		</div>
 	);
 }
